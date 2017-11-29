b444317159710e996f49a6e09964bd6108c188cb
Tatiana Fesenko <tatiana.fesenko@gmail.com>
2017-11-29 16:49:27 -0500

    Mavenize YAML DSL toolkit

--- com.reprezen.kaizen.dsltoolkit.universal/src/com/reprezen/kaizen/editor/jsonschema/editor/JsonSchema4Document.java
+++ com.reprezen.kaizen.dsltoolkit.universal/src/com/reprezen/kaizen/editor/jsonschema/editor/JsonSchema4Document.java
@@ -1,5 +1,7 @@
 package com.reprezen.kaizen.editor.jsonschema.editor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.reprezen.kaizen.editor.jsonschema.Activator;
 import com.reprezen.kaizen.editor.jsonschema.schema.JsonSchema4Schema;
 import com.reprezen.swagedit.core.editor.JsonDocument;
@@ -11,7 +13,7 @@ public class JsonSchema4Document extends JsonDocument {
 	}
 
     public JsonSchema4Document(JsonSchema4Schema schema) {
-        super(io.swagger.util.Yaml.mapper(), schema);
+        super(new ObjectMapper(new YAMLFactory()), schema);
     }
 
 }
